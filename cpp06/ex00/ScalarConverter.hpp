e2153141dcd7554f06dcbba5bc43d768b5266670
Gaebel Hwa <ghwa@u82z03s02.42kl.edu.my>
2025-01-10 14:54:09 +0800

    splsd

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 13:10:24 by ghwa              #+#    #+#             */
+/*   Updated: 2025/01/10 14:44:14 by ghwa             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter {
+public:
+	static void convert(const std::string &literal);
+
+	ScalarConverter() = delete;
+	ScalarConverter(const ScalarConverter &) = delete;
+	ScalarConverter &operator=(const ScalarConverter &) = delete;
+	~ScalarConverter() = default;
+};
+
+#endif
