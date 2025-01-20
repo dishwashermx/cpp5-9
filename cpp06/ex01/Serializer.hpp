5a3b3ce7283cb5e4da128c657518ecf12bd84b95
Gaebel Hwa <ghwa@u82z05s02.42kl.edu.my>
2025-01-20 15:06:19 +0800

    spks

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:39:03 by ghwa              #+#    #+#             */
-/*   Updated: 2025/01/17 15:47:22 by ghwa             ###   ########.fr       */
+/*   Updated: 2025/01/20 10:10:54 by ghwa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class Serializer {
 	private:
 		Serializer();
-		Serializer(const std string &str);
+		Serializer(const std::string &str);
 		Serializer(const Serializer &other);
 		Serializer &operator=(const Serializer &other);
 		~Serializer();
