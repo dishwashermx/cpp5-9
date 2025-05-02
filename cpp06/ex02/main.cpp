48405c5126d1946ee96646e8b371be4df3cc70f0
Gaebel Hwa <ghwa@u91z03s01.42kl.edu.my>
2025-05-02 11:02:34 +0800

    idk

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,16 +6,14 @@
 /*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 09:57:53 by ghwa              #+#    #+#             */
-/*   Updated: 2025/04/30 20:19:30 by ghwa             ###   ########.fr       */
+/*   Updated: 2025/04/11 10:35:28 by ghwa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
-#include <ctime>
-
 
 int main(void) {
-	::srand(static_cast<unsigned int>(std::clock()));
+	srand(time(0));
 	Base base;
 	Base* obj = base.generate();
 
