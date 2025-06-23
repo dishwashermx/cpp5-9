88afe5b17cf449243c7ff7de3b47ae17008694bc
Gaebel Hwa <gaebelhdz@gmail.com>
2025-06-23 09:46:05 +0800

    mutant stack done

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,29 @@
 /*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 17:07:26 by ghwa              #+#    #+#             */
-/*   Updated: 2025/06/20 17:07:27 by ghwa             ###   ########.fr       */
+/*   Updated: 2025/06/23 09:42:04 by ghwa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+public:
+	MutantStack() {}
+	MutantStack(const MutantStack<T>& other) : std::stack<T>(other) {}
+	MutantStack<T>& operator=(const MutantStack<T>& other) {
+		std::stack<T>::operator=(other);
+		return *this;
+	}
+	~MutantStack() {}
+
+	//exposing the underlying iterators and giving it a name so my class can use it
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+	const_iterator begin() const { return this->c.begin(); }
+	const_iterator end() const { return this->c.end(); }
+};
