9445b817944526d0e6c7590eb5ac5c3c1e915e91
Gaebel Hwa <gaebelhdz@gmail.com>
2025-06-23 10:22:22 +0800

    refresh

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,16 +6,12 @@
 /*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 13:10:24 by ghwa              #+#    #+#             */
-/*   Updated: 2025/04/24 14:05:27 by ghwa             ###   ########.fr       */
+/*   Updated: 2025/06/23 10:03:59 by ghwa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
-#define RESET   "\033[0m"
-#define BOLD    "\033[1m"
-#define CYAN    "\033[36m"
-#define GREY    "\033[90m"
 #include <iostream>
 
 template <typename T>
@@ -44,7 +40,7 @@ class Array {
 		~Array() {
 			delete[] _data;
 		};
-		T& operator[](unsigned int index) {
+		const T& operator[](unsigned int index) {
 			if (index >= _size) {
 				throw std::out_of_range("Index out of bounds!");
 			}
