6d3520c9e4c23f53d43081222b6aefd580c23447
Gaebel Hwa <ghwa@u82z05s01.42kl.edu.my>
2025-07-02 15:53:35 +0800

    sda

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 12:12:27 by ghwa              #+#    #+#             */
-/*   Updated: 2025/06/30 15:08:20 by ghwa             ###   ########.fr       */
+/*   Updated: 2025/07/02 13:16:40 by ghwa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int main(int argc, char** argv) {
 		Bit.processInput(argv[1]);
 	}
 	catch (std::exception &e) {
-		std::cerr << "Error: " << e.what() << '\n';
+		std::cerr << "Error: " << e.what() << std::endl;
 	}
 	return (0);
 }
